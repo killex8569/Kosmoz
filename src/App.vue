421c4e1a5972a85a68e57f61169615c4b2bf61eb
killex8569 <alexandreanette@gmail.com>
2025-12-02 11:27:11 +0100

    Feat: Add Tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,33 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-8">
+    <!-- Logos existants -->
+    <div class="flex space-x-6 mb-8">
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+
+    <!-- HelloWorld existant -->
+    <HelloWorld msg="Vite + Vue" />
+
+    <!-- Test Tailwind CSS -->
+    <div class="mt-12 p-6 bg-blue-200 rounded-lg shadow-lg text-center">
+      <h1 class="text-4xl font-bold text-red-600 underline">
+        Test Tailwind CSS
+      </h1>
+      <p class="mt-4 text-gray-700">
+        Si tu vois ce style coloré et arrondi, Tailwind fonctionne.
+      </p>
+      <button class="mt-6 px-4 py-2 bg-green-500 text-white font-semibold rounded hover:bg-green-600 transition">
+        Bouton Tailwind
+      </button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
